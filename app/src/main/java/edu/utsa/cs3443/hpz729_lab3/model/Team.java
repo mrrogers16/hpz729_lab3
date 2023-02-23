5fc0104936d57e4d6c7aebb13aa0f4c3c317b584
mclarke16 <107970544+mclarke16@users.noreply.github.com>
2023-02-22 23:06:57 -0600

    Started on the bones

--- app/src/main/java/edu/utsa/cs3443/hpz729_lab3/model/Team.java
+++ app/src/main/java/edu/utsa/cs3443/hpz729_lab3/model/Team.java
@@ -1,4 +1,32 @@
 package edu.utsa.cs3443.hpz729_lab3.model;
 
+import java.util.ArrayList;
+
 public class Team {
+    private ArrayList<Avenger> avengerList = new ArrayList<Avenger>();
+}
+
+    /**
+     * @param filename
+     */
+    public void loadAvengers(String filename) {
+        //@TODO
+    }
+
+    /**
+     * The getAvenger method takes in a string of type avenger and returns and Avenger object
+     * @param avenger
+     * @return Avenger
+     */
+public Avenger getAvenger(String avenger) {
+    return Avenger;
+}
+
+    /**
+     * @return
+     */
+@Override
+public String toString()
+{
+    //TODO create toString
 }
